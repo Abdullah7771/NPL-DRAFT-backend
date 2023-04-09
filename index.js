da8eb955fb0207b36d459ee3d9839177b862825a
Abdullah7771 <65004459+Abdullah7771@users.noreply.github.com>
2023-04-10 01:05:58 +0500

    js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 
   // Catch all routes and return index.html
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
