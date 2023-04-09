0ccc2e956d8b9927d511907b97f717d0a4c0c101
Abdullah7771 <65004459+Abdullah7771@users.noreply.github.com>
2023-04-09 16:18:21 +0500

    s

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ app.use('/api/round', require('./routes/round'));
 //   module.exports=require('./dev');
 // }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
+
+app.get('/api/*', (req, res) => {
+  res.status(404).send({ message: 'API endpoint not found' });
+});
+
+
 
 if ( process.env.NODE_ENV == "production"){
 
